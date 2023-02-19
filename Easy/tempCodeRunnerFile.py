5deae46dcad1714b24084058dd1363558cd06075
ChawKokFei <80729400+ChawKokFei@users.noreply.github.com>
2023-02-19 22:28:53 +0800

    Remove tmp file

--- Easy/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-            # if number[index + 1] != None:
-            #     if number[index + 1] > number:
-            #         value += (number[index + 1] - number)
-            #     else:
-            #         value += number
-            # else:
-            #     value += number
