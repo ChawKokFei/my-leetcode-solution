fba2511664d28921b5e780b2ae6161bf36cf6af7
ChawKokFei <80729400+ChawKokFei@users.noreply.github.com>
2023-03-02 06:47:34 +0800

    Medium Q912

---
+++ Medium/q912_sort_an_array.py
@@ -0,0 +1,41 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.mergeSort(nums)
+
+        return nums
+
+    # merge sort
+    # time complexity O(n log n)
+    # space complexity O(n)
+    def mergeSort(self, array: list[int]):
+        if len(array) <= 1:
+            return
+
+        mid = len(array) // 2
+        leftArray = array[:mid]
+        rightArray = array[mid:]
+
+        self.mergeSort(leftArray)
+        self.mergeSort(rightArray)
+
+        leftPointer = rightPointer = oriPointer = 0
+
+        while leftPointer < len(leftArray) and rightPointer < len(rightArray):
+            if leftArray[leftPointer] <= rightArray[rightPointer]:
+                array[oriPointer] = leftArray[leftPointer]
+                leftPointer += 1
+                oriPointer += 1
+            else:
+                array[oriPointer] = rightArray[rightPointer]
+                rightPointer += 1
+                oriPointer += 1
+
+        if leftPointer < len(leftArray):
+            for num in leftArray[leftPointer:]:
+                array[oriPointer] = num
+                oriPointer += 1
+
+        if rightPointer < len(rightArray):
+            for num in rightArray[rightPointer:]:
+                array[oriPointer] = num
+                oriPointer += 1
